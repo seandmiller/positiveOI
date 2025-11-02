93f80789bb8a1e8db844f7066505d9399255a9eb
Licht <76960008+seandmiller@users.noreply.github.com>
2025-11-01 18:32:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def get_metrics(ticker):
         
         return jsonify(response)
     except ValueError as e:
+        print(e)
         return str(e), 400
 if __name__ == '__main__':
     app.run(debug=True)
