f1b1995ed05b0556e4cad68a7149c42e5bd0ce56
Licht <76960008+seandmiller@users.noreply.github.com>
2025-11-26 22:50:58 -0800

    Update sent_tracker.py

--- py_tools/sent_tracker.py
+++ py_tools/sent_tracker.py
@@ -10,7 +10,7 @@ class HeadlineSentimentAnalyzer:
     def __init__(self):
         """Initializes the analyzer to use the Hugging Face Inference API."""
         
-        self.API_URL = "https://router.huggingface.co/hf-inference/models/distilbert-base-uncased-finetuned-sst-2-english"
+        self.API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased"
         
         hf_token = os.environ.get("HF_TOKEN")
   
